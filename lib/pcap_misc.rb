ahobson/ruby-pcap

commit 272f03ff3b5bf79829f80c2febd004904d64006e

Added source, destination mac address methods to TCP Packet.

lib/pcap_misc.rb
@@ -34,6 +34,18 @@ module Pcap
     def to_s
       "#{src}:#{sport} > #{dst}:#{dport} #{tcp_flags_s}"
     end
+
+    def src_mac_address
+      return unpack_hex_string(raw_data[6, 12])
+    end
+
+    def dst_mac_address
+      return unpack_hex_string(raw_data[0, 6])
+    end
+
+    def unpack_hex_string(hex)
+      return hex.unpack('H2H2H2H2H2H2').reverse.join('')
+    end
   end
 
   class UDPPacket

commit 0ad41d0684c2ec4c2a6b604f7aafbaf9f0459dcc

Destination and Source mac addresses are not in reverse order.
This commit fixes a bug introduced in 272f03ff3b5bf79829f80c2febd004904d64006e.

lib/pcap_misc.rb
@@ -44,7 +44,7 @@ module Pcap
     end
 
     def unpack_hex_string(hex)
-      return hex.unpack('H2H2H2H2H2H2').reverse.join('')
+      return hex.unpack('H2H2H2H2H2H2').join('')
     end
   end
 